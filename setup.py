60515adf2f431164fedef7fa5515a2e637b50bcb
Junbo Jia <68803699+Just-Jia@users.noreply.github.com>
2022-01-25 21:40:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ccnet", # Replace with your own username
-    version="1.0.1",
+    version="1.0.3",
     author="Junbo Jia",
     author_email="junbo_jia@163.com",
     description="A network-based single-cell RNA-seq data analysis library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Just-Jia/ccNet",
-    packages=setuptools.find_packages(where="ccnet"),
-    package_dir={"": "ccnet"},
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
